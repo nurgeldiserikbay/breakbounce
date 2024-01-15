b026d14df874354c82a99f292e15027e275e7a68
nurgeldiserikbay <nurikserikbay1995@gmail.com>
2024-01-15 23:24:06 +0600

    save changes

--- main.js
+++ main.js
@@ -10,25 +10,12 @@ class GameOpt {
     this.pixelsOnMm = 3
     this.pixelsOnMetr = 3000
 
+    this.ball = null
+
     this.obj = {
       frictionFactor: 0.5,
-    }
-
-    this.ball = null
-    this.pointerStart = null
-    this.lastPointerTime = 0
-    this.forceMultiplier = 500
-    this.forceMultiplierDevice = 500
-    this.collisionSound = null
-    this.bounceFactor = 0.5
-
-    this.gravityPixels = 300
-
-    this.orientation = {
-      freq: 0,
-      alpha: 0,
-      beta: 0,
-      gamma: 0,
+      bounceFactor: 0.5,
+      dragFactor: 0.98,
     }
 
     this.acceleration = {
@@ -37,22 +24,34 @@ class GameOpt {
       y: 0,
       z: 0,
     }
+
+    this.pointerStart = null
+    this.lastPointerTime = 0
+    this.forceMultiplier = 500
+    this.forceMultiplierDevice = 500
+    this.collisionSound = null
+
+    this.orientation = {
+      freq: 0,
+      alpha: 0,
+      beta: 0,
+      gamma: 0,
+    }
     
     this.setPixelsCount()
-    this.obj.frictionFactor = Math.min(Number(prompt('Friction factor value?') || 1), 1)
     this.gravityController()
   }
 
-  handleOrientation(event) {
-    if (event.timeStamp - this.orientation.freq > FREQ) {
-      this.orientation = {
-        freq: event.timeStamp,
-        alpha: event.alpha,
-        beta: event.beta,
-        gamma: event.gamma,
-      }
-    }
-  }
+  // handleOrientation(event) {
+  //   if (event.timeStamp - this.orientation.freq > FREQ) {
+  //     this.orientation = {
+  //       freq: event.timeStamp,
+  //       alpha: event.alpha,
+  //       beta: event.beta,
+  //       gamma: event.gamma,
+  //     }
+  //   }
+  // }
 
   preload() {
     this.load.image('ball', './assets/football.png')
@@ -60,37 +59,27 @@ class GameOpt {
   }
   
   create() {
-
     this.ball = this.physics.add.sprite(WIDTH / 2, HEIGHT / 2, 'ball')
     this.ball.setScale(0.15, 0.15)
     this.ball.setInteractive()
     this.ball.setCollideWorldBounds(true)
-
-    this.ball.setBounce(this.bounceFactor, this.bounceFactor)
-
-    this.collisionSound = this.sound.add('collision')
-
-    this.input.on('pointerdown', this.handleSwipeStart.bind(this))
-    this.input.on('pointermove', this.handleSwipeMove.bind(this))
-    this.input.on('pointerup', this.handleSwipeEnd.bind(this))
-
-    window.addEventListener('deviceorientation', this.handleOrientation.bind(this))
+    this.ball.setBounce(this.obj.bounceFactor, this.obj.bounceFactor)
+    this.ball.setDamping(true)
+    this.ball.setDrag(this.obj.dragFactor)
+    
     window.addEventListener('devicemotion', this.handleDeviceAcceleration.bind(this))
     window.addEventListener('click', () => {
       console.log('orientation', )
       console.log('acceleration', this.acceleration)
     })
-  }
 
-  handleDeviceAcceleration(event) {
-    if (event.timeStamp - this.acceleration.freq > FREQ) {
-      this.acceleration = {
-        freq: event.timeStamp,
-        x: event.acceleration.x,
-        y: event.acceleration.y,
-        z: event.acceleration.z,
-      }
-    }
+    this.collisionSound = this.sound.add('collision')
+
+    // this.input.on('pointerdown', this.handleSwipeStart.bind(this))
+    // this.input.on('pointermove', this.handleSwipeMove.bind(this))
+    // this.input.on('pointerup', this.handleSwipeEnd.bind(this))
+
+    // window.addEventListener('deviceorientation', this.handleOrientation.bind(this))
   }
   
   update() {
@@ -107,59 +96,57 @@ class GameOpt {
     this.collisionSound.play()
   }
 
-  handleSwipeStart(pointer) {
-    this.pointerStart = {
-      x: pointer.position.x,
-      y: pointer.position.y2
-    }
-
-    this.lastPointerTime = this.time.now
-  }
-
-  handleSwipeMove(pointer) {
-    if (this.pointerStart) {
-      const currentTime = this.time.now
-      const deltaTime = currentTime - this.lastPointerTime
-
-      const swipeDistanceX = pointer.position.x - this.pointerStart.x
-      const swipeDistanceY = pointer.position.y - this.pointerStart.y
-
-      const angle = Math.atan2(swipeDistanceY, swipeDistanceX)
-
-      const swipeSpeed = Math.sqrt(swipeDistanceX ** 2 + swipeDistanceY ** 2) / deltaTime
-  
-      const velocityX = Math.cos(angle) * swipeSpeed * this.forceMultiplier
-      const velocityY = Math.sin(angle) * swipeSpeed * this.forceMultiplier
-
-      this.ball.setVelocity(velocityX, velocityY)
-    }
-  }
-
-  handleSwipeEnd() {
-    this.pointerStart = null
-  }
-
-  // handleDeviceAcceleration(event) {
-  //   let accelerationX = 0
-  //   let accelerationY = 0
-
-  //   if (Math.abs(accelerationX) > MOVE_MIN_VAL) {
-  //     // console.log(`x = ${accelerationX};`)
-  //     accelerationX = -1 * this.forceMultiplierDevice * event.acceleration.x
-  //     this.ball.setVelocityX(accelerationX)
+  // handleSwipeStart(pointer) {
+  //   this.pointerStart = {
+  //     x: pointer.position.x,
+  //     y: pointer.position.y2
   //   }
-  //   if (Math.abs(accelerationY) > MOVE_MIN_VAL) {
-  //     // console.log(`y = ${accelerationY};`)
-  //     accelerationY = this.forceMultiplierDevice * event.acceleration.y
-  //     this.ball.setVelocityY(accelerationY)
+
+  //   this.lastPointerTime = this.time.now
+  // }
+
+  // handleSwipeMove(pointer) {
+  //   if (this.pointerStart) {
+  //     const currentTime = this.time.now
+  //     const deltaTime = currentTime - this.lastPointerTime
+
+  //     const swipeDistanceX = pointer.position.x - this.pointerStart.x
+  //     const swipeDistanceY = pointer.position.y - this.pointerStart.y
+
+  //     const angle = Math.atan2(swipeDistanceY, swipeDistanceX)
+
+  //     const swipeSpeed = Math.sqrt(swipeDistanceX ** 2 + swipeDistanceY ** 2) / deltaTime
+  
+  //     const velocityX = Math.cos(angle) * swipeSpeed * this.forceMultiplier
+  //     const velocityY = Math.sin(angle) * swipeSpeed * this.forceMultiplier
+
+  //     this.ball.setVelocity(velocityX, velocityY)
   //   }
   // }
 
+  // handleSwipeEnd() {
+  //   this.pointerStart = null
+  // }
+
+  handleDeviceAcceleration(event) {
+    if (event.timeStamp - this.acceleration.freq > FREQ) {
+      this.acceleration = {
+        freq: event.timeStamp,
+        x: event.acceleration.x,
+        y: event.acceleration.y,
+        z: event.acceleration.z,
+      }
+
+      const xVel = -1 * this.acceleration.x * this.pixelsOnMetr * (this.obj.frictionFactor || 1)
+      const yVel = -1 * this.acceleration.y * this.pixelsOnMetr * (this.obj.frictionFactor || 1)
+
+      this.ball.setVelocity(xVel, yVel)
+    }
+  }
+
   gravityController() {
     const gravitySensor = new GravitySensor({ frequency: 30 })
-    console.log('pixelsOnMm', this.pixelsOnMm)
-    console.log('pixelsOnMetr', this.pixelsOnMetr)
-    console.log('frictionFactor', this.obj.frictionFactor)
+
     gravitySensor.addEventListener('reading', () => {
       const x = Math.round(gravitySensor.x * this.pixelsOnMetr * (this.obj.frictionFactor || 1))
       const y = Math.round(gravitySensor.y * this.pixelsOnMetr * (this.obj.frictionFactor || 1))
